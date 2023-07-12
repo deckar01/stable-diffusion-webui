eb7a650711335cd36f8678c8504f028e3dd478e6
Jared Deckard <jared@shademaps.com>
2023-07-11 20:19:08 -0500

    Fix gradio live yield in the call queue

--- modules/call_queue.py
+++ modules/call_queue.py
@@ -1,4 +1,5 @@
 from functools import wraps
+from types import GeneratorType
 import html
 import threading
 import time
@@ -34,16 +35,19 @@ def wrap_gradio_gpu_call(func, extra_outputs=None):
             res = [None, '', '', '']
             try:
                 res = func(*args, **kwargs)
+                if not isinstance(res, GeneratorType):
+                    res = [res]
+                yield from res
                 progress.record_results(id_task, res)
             except Exception as e:
                 shared.log.error(f"Exception: {e}")
                 shared.log.error(f"Arguments: args={str(args)[:10240]} kwargs={str(kwargs)[:10240]}")
                 errors.display(e, 'gradio call')
-                res[-1] = f"<div class='error'>{html.escape(str(e))}</div>"
+                error_html = f"<div class='error'>{html.escape(str(e))}</div>"
+                yield (*extra_outputs, error_html)
             finally:
                 progress.finish_task(id_task)
             shared.state.end()
-        return res
     return wrap_gradio_call(f, extra_outputs=extra_outputs, add_stats=True)
 
 
@@ -63,8 +67,11 @@ def wrap_gradio_call(func, extra_outputs=None, add_stats=False):
                 msg = "No result returned from function"
                 shared.log.warning(msg)
                 res = [None, '', '', f"<div class='error'>{html.escape(msg)}</div>"]
-            else:
-                res = list(res)
+            if not isinstance(res, GeneratorType):
+                res = [res]
+            for r in res:
+                # last item is always HTML
+                yield (*r[:-1], r[-1] + get_stats(add_stats, t, run_memmon))
             if shared.cmd_opts.profile:
                 pr.disable()
                 s = io.StringIO()
@@ -76,28 +83,43 @@ def wrap_gradio_call(func, extra_outputs=None, add_stats=False):
             shared.state.job_count = 0
             if extra_outputs_array is None:
                 extra_outputs_array = [None, '']
-            res = extra_outputs_array + [f"<div class='error'>{html.escape(type(e).__name__+': '+str(e))}</div>"]
+
+            error_message = f'{type(e).__name__}: {e}'
+            error_html = f"<div class='error'>{html.escape(error_message)}</div>"
+            stats = get_stats(add_stats, t, run_memmon)
+            yield (*extra_outputs_array, error_html + stats)
+
         shared.state.skipped = False
         shared.state.interrupted = False
         shared.state.paused = False
         shared.state.job_count = 0
-        if not add_stats:
-            return tuple(res)
-        elapsed = time.perf_counter() - t
-        elapsed_m = int(elapsed // 60)
-        elapsed_s = elapsed % 60
-        elapsed_text = f"{elapsed_s:.2f}s"
-        if elapsed_m > 0:
-            elapsed_text = f"{elapsed_m}m "+elapsed_text
+
         if run_memmon:
-            mem_stats = {k: -(v//-(1024*1024)) for k, v in shared.mem_mon.stop().items()}
-            active_peak = mem_stats['active_peak']
-            reserved_peak = mem_stats['reserved_peak']
-            sys_peak = mem_stats['system_peak']
-            sys_total = mem_stats['total']
-            vram_html = f" | <p class='vram'>GPU active {active_peak} MB reserved {reserved_peak} MB | System peak {sys_peak} MB total {sys_total} MB</p>"
-        else:
-            vram_html = ''
-        res[-1] += f"<div class='performance'><p class='time'>Time taken: {elapsed_text}</p>{vram_html}</div>"
-        return tuple(res)
+            shared.mem_mon.stop()
+
     return f
+
+def get_stats(add_stats, t, run_memmon):
+    if not add_stats:
+        return ""
+
+    elapsed = time.perf_counter() - t
+    elapsed_m = int(elapsed // 60)
+    elapsed_s = elapsed % 60
+    elapsed_text = f"{elapsed_s:.2f}s"
+    if elapsed_m > 0:
+        elapsed_text = f"{elapsed_m}m "+elapsed_text
+
+    if run_memmon:
+        mem_stats = {k: -(v//-(1024*1024)) for k, v in shared.mem_mon.read().items()}
+        active_peak = mem_stats['active_peak']
+        reserved_peak = mem_stats['reserved_peak']
+        sys_peak = mem_stats['system_peak']
+        sys_total = mem_stats['total']
+        sys_pct = round(sys_peak/max(sys_total, 1) * 100, 2)
+
+        vram_html = f" | <p class='vram'>GPU active {active_peak} MB reserved {reserved_peak} MB | System peak {sys_peak} MB total {sys_total} MB</p>"
+    else:
+        vram_html = ''
+
+    return f"<div class='performance'><p class='time'>Time taken: {elapsed_text}</p>{vram_html}</div>"
